71a850f4e3b94f44500427664f9ffd47e11a509e
ilyanfraimbault <ilyanfraimbault@gmail.com>
2026-02-04 11:47:38 +0100

    feat: add PostgreSQL support and update project configurations

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,8 +1,23 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<TrueMainDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("TrueMain");
+
+    if (string.IsNullOrWhiteSpace(connectionString))
+    {
+        throw new InvalidOperationException(
+            "Missing connection string. Add ConnectionStrings:TrueMain to user secrets.");
+    }
+
+    options.UseNpgsql(connectionString);
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
